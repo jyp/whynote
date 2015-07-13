11b51f99e563f70ac6c56d98f10a6fdaddc4761b
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2015-07-13 22:43:21 +0200

    nix def

---
+++ default.nix
@@ -0,0 +1,17 @@
+{ mkDerivation, aeson, base, bytestring, cairo, configurator
+, containers, glib, gtk3, lens, mtl, old-locale, stdenv, time
+, transformers, unordered-containers, vector, yap
+}:
+mkDerivation {
+  pname = "whynote";
+  version = "1";
+  src = ./.;
+  isLibrary = false;
+  isExecutable = true;
+  buildDepends = [
+    aeson base bytestring cairo configurator containers glib gtk3 lens
+    mtl old-locale time transformers unordered-containers vector yap
+  ];
+  description = "A minimal note-taking program";
+  license = stdenv.lib.licenses.gpl3;
+}
